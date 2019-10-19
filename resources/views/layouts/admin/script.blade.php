9a526a3f2fe5dcf7202ab2d3c0e3f8d8966ddc93
Vanh <tranvanh0905@gmail.com>
2019-10-19 09:50:30 +0700

    add Admin

--- resources/views/layouts/admin/script.blade.php
+++ resources/views/layouts/admin/script.blade.php
@@ -1,58 +1,35 @@
-<!-- jQuery 3 -->
-<script src="adminLte/bower_components/jquery/dist/jquery.min.js"></script>
-<!-- jQuery UI 1.11.4 -->
-<script src="adminLte/bower_components/jquery-ui/jquery-ui.min.js"></script>
-<!-- Resolve conflict in jQuery UI tooltip with Bootstrap tooltip -->
-<script>
-    $.widget.bridge('uibutton', $.ui.button);
-</script>
-<!-- Bootstrap 3.3.7 -->
-<script src="adminLte/bower_components/bootstrap/dist/js/bootstrap.min.js"></script>
-<!-- Morris.js charts -->
-<script src="adminLte/bower_components/raphael/raphael.min.js"></script>
-<script src="adminLte/bower_components/morris.js/morris.min.js"></script>
-<!-- Sparkline -->
-<script src="adminLte/bower_components/jquery-sparkline/dist/jquery.sparkline.min.js"></script>
-<!-- jvectormap -->
-<script src="adminLte/plugins/jvectormap/jquery-jvectormap-1.2.2.min.js"></script>
-<script src="adminLte/plugins/jvectormap/jquery-jvectormap-world-mill-en.js"></script>
-<!-- jQuery Knob Chart -->
-<script src="adminLte/bower_components/jquery-knob/dist/jquery.knob.min.js"></script>
-<!-- daterangepicker -->
-<script src="adminLte/bower_components/moment/min/moment.min.js"></script>
-<script src="adminLte/bower_components/bootstrap-daterangepicker/daterangepicker.js"></script>
-<!-- datepicker -->
-<script src="adminLte/bower_components/bootstrap-datepicker/dist/js/bootstrap-datepicker.min.js"></script>
-<!-- Bootstrap WYSIHTML5 -->
-<script src="adminLte/plugins/bootstrap-wysihtml5/bootstrap3-wysihtml5.all.min.js"></script>
-<!-- Slimscroll -->
-<script src="adminLte/bower_components/jquery-slimscroll/jquery.slimscroll.min.js"></script>
-<!-- FastClick -->
-<script src="adminLte/bower_components/fastclick/lib/fastclick.js"></script>
-<!-- AdminLTE App -->
-<script src="adminLte/dist/js/adminlte.min.js"></script>
-<!-- AdminLTE dashboard demo (This is only for demo purposes) -->
-<script src="adminLte/dist/js/pages/dashboard.js"></script>
-<!-- AdminLTE for demo purposes -->
-<script src="adminLte/dist/js/demo.js"></script>
-<script src="https://unpkg.com/sweetalert/dist/sweetalert.min.js"></script>
-<script>
-    $(function () {
-        $('.delete-btn').on('click', function () {
-            var removeUrl = $(this).attr('linkurl');
-            swal({
-                title: "Are you sure delete ?",
-                text: "Once deleted, you will not be able to recover this imaginary file!",
-                icon: "warning",
-                buttons: true,
-                dangerMode: true,
-            })
-                .then((willDelete) => {
-                    if (willDelete) {
-                        window.location.href = removeUrl;
-                    }
-                });
-        });
-    });
-</script>
+<script data-cfasync="false" src="../../../cdn-cgi/scripts/5c5dd728/cloudflare-static/email-decode.min.js"></script>
+<script type="d1b00181c87e117ceab38751-text/javascript" src="../admin-dek//files/bower_components/jquery/js/jquery.min.js"></script>
+<script type="d1b00181c87e117ceab38751-text/javascript" src="../admin-dek/files/bower_components/jquery-ui/js/jquery-ui.min.js"></script>
+<script type="d1b00181c87e117ceab38751-text/javascript" src="../admin-dek/files/bower_components/popper.js/js/popper.min.js"></script>
+<script type="d1b00181c87e117ceab38751-text/javascript" src="../admin-dek/files/bower_components/bootstrap/js/bootstrap.min.js"></script>
 
+<script src="../admin-dek/files/assets/pages/waves/js/waves.min.js" type="d1b00181c87e117ceab38751-text/javascript"></script>
+
+<script type="d1b00181c87e117ceab38751-text/javascript" src="../admin-dek/files/bower_components/jquery-slimscroll/js/jquery.slimscroll.js"></script>
+
+<script src="../admin-dek/files/assets/pages/chart/float/jquery.flot.js" type="d1b00181c87e117ceab38751-text/javascript"></script>
+<script src="../admin-dek/files/assets/pages/chart/float/jquery.flot.categories.js" type="d1b00181c87e117ceab38751-text/javascript"></script>
+<script src="../admin-dek/files/assets/pages/chart/float/curvedLines.js" type="d1b00181c87e117ceab38751-text/javascript"></script>
+<script src="../admin-dek/files/assets/pages/chart/float/jquery.flot.tooltip.min.js" type="d1b00181c87e117ceab38751-text/javascript"></script>
+
+<script src="../admin-dek/files/bower_components/chartist/js/chartist.js" type="d1b00181c87e117ceab38751-text/javascript"></script>
+
+<script src="../admin-dek/files/assets/pages/widget/amchart/amcharts.js" type="d1b00181c87e117ceab38751-text/javascript"></script>
+<script src="../admin-dek/files/assets/pages/widget/amchart/serial.js" type="d1b00181c87e117ceab38751-text/javascript"></script>
+<script src="../admin-dek/files/assets/pages/widget/amchart/light.js" type="d1b00181c87e117ceab38751-text/javascript"></script>
+
+<script src="../admin-dek/files/assets/js/pcoded.min.js" type="d1b00181c87e117ceab38751-text/javascript"></script>
+<script src="../admin-dek/files/assets/js/vertical/vertical-layout.min.js" type="d1b00181c87e117ceab38751-text/javascript"></script>
+<script type="d1b00181c87e117ceab38751-text/javascript" src="../admin-dek/files/assets/pages/dashboard/custom-dashboard.min.js"></script>
+<script type="d1b00181c87e117ceab38751-text/javascript" src="../admin-dek/files/assets/js/script.min.js"></script>
+
+<script src="../admin-dek/files/assets/js/rocket-loader.min.js"></script>
+<script type="d1b00181c87e117ceab38751-text/javascript">
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'UA-23581568-13');
+
+</script>
